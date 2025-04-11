968451db50b51a439afebc0e0d4be6583f04ef83
eguediche <eguediche@edenschool.fr>
2025-04-11 15:59:31 +0200

    FINISH

--- script.js
+++ script.js
@@ -6,22 +6,43 @@ const ipLocation = document.querySelector("#ipLocation");
 const ipTimeZone = document.querySelector("#ipTimeZone");
 const ipIsp = document.querySelector("#ipIsp");
 
-    let IP = input.value
+let map = L.map('map').setView([51.505, -0.09], 13);
+let marker = L.marker([51.5, -0.09]).addTo(map);
 
-input.addEventListener("input", () => {
-    console.log(IP);
+L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    maxZoom: 19,
+    attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
+}).addTo(map);
+
+input.addEventListener("keydown", (e) => {
+    if (e.key === "Enter") {
+        caca();
+    }
 }) 
 
 
 btn.addEventListener("click", () => {
-    async function main() {
-        const API = await fetch("https://geo.ipify.org/api/v2/country,city?apiKey=at_wqy9tUA1kyah20Z1MtTJNwG5csGTi&", {
-            method: "GET",
-            
-        });
-        const result = await API.json();
-        console.log(result);
-    }
-    
-    main();
+    caca();
 })
+
+
+function caca () {
+
+async function main() {
+    const data = await fetch("https://geo.ipify.org/api/v2/country,city?apiKey=at_wqy9tUA1kyah20Z1MtTJNwG5csGTi&ipAddress=" + input.value, {
+        method: "GET",
+    });
+    const result = await data.json();
+    marker.remove(marker);
+    marker = L.marker([result.location.lat, result.location.lng]).addTo(map)
+    ipAddress.textContent = result.ip;
+    ipLocation.textContent = result.location.city + ", " + result.location.country + " " + result.location.postalCode;
+    ipTimeZone.textContent = "UTC " + result.location.timezone;
+    ipIsp.textContent = result.isp
+    map.setView([result.location.lat, result.location.lng], 13);
+}
+
+main();
+
+
+ }
